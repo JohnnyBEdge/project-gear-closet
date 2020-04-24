96e59f0f47ea24d7b1f1142e91957465b59eeede
John Martin <jrmartin182@gmail.com>
2020-04-24 14:45:38 -0600

    deployment of react UI

---
+++ gear-closet-api/public/precache-manifest.d3ca6d1506d80c2ace3dfbe3b84eedd2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cfc9a46e15ce2da10ad149b192b18a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca7297b1c8cab944fa5f",
+    "url": "/static/css/main.17466fd1.chunk.css"
+  },
+  {
+    "revision": "0a95f611173087b83ce3",
+    "url": "/static/js/2.500c49a1.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/static/js/2.500c49a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca7297b1c8cab944fa5f",
+    "url": "/static/js/main.06447522.chunk.js"
+  },
+  {
+    "revision": "ae7ffe5a3f8fc3725f4c",
+    "url": "/static/js/runtime-main.a70954f0.js"
+  },
+  {
+    "revision": "e0d2812e7219dfc8c76dc82a987c817f",
+    "url": "/static/media/plus.e0d2812e.png"
+  }
+]);
